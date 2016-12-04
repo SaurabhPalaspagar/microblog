10a3b060537abb7d58e3ec4a8d7327f930857a02
Saurabh V. Palaspagar <saurabh.palaspagar@outlook.com>
2016-12-03 19:04:37 -0500

    User Profile & Avators

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ class User(db.Model):
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     @property
     def is_authenticated(self):
@@ -26,6 +28,9 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+		
+    def avatar(self, size):
+        return 'https://github.com/SaurabhPalaspagar/saurabhpalaspagar.github.io/blob/master/assets/images/myimage.jpg'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
